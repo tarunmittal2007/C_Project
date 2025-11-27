96a7a25b2d32bec3c99d1df2f618943ebd9e38da
tarunmittal2007 <tarunmittal8572@gmail.com>
2025-11-28 00:10:01 +0530

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 #include "utils.h"
 
+// funtion to input the date from the user 
 void inputDate(char *date) 
 {
-    printf("Enter Date (DD-MM-YYYY): ");
-    scanf("%s", date);
+printf("Enter Date in the format (DD-MM-YYYY): ");
+scanf("%s",date);
 }
