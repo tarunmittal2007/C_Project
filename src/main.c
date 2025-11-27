39401e183aa8ca953445b6d14d4c1b776e10a594
tarunmittal2007 <tarunmittal8572@gmail.com>
2025-11-28 00:09:52 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,47 +1,42 @@
-#include <stdio.h>
+#include<stdio.h> 
+#include<string.h>
+#include<stdlib.h>
 #include "utils.h"
 #include "tollsystem.h"
 
 int main() 
 {
-    char date[20];
-    inputDate(date);
+char date[20];
+inputDate(date);
+int choice;
 
-    int choice;
+do {
+printf("\n===== Vehicle Toll Control System =====\n");
+printf("1. Toll collection for vehicle \n");
+printf("2. Show vehicle History by Date\n");
+printf("3. Daily amount Receipt Summary\n");
+printf("4. Exit \n");
+printf("Enter your choice: ");
+scanf("%d",&choice);
+// here the user will enter his choice about the task he wants to perform and the same function wil be executed accordingly
+switch(choice) 
+{
+case 1: 
+TollCollection(date); 
+break;
+case 2: 
+ShowHistory(); 
+break;
+case 3: 
+dailyReceipt(); 
+break;
+case 4: 
+printf("Thank You \n"); 
+break;
+default: 
+printf("Invalid choice selected! Try again\n");
+}
+}while(choice != 4);// the loop will be executed until the user select 4 
 
-    do {
-        printf("\n===== Vehicle Toll Control System =====\n");
-        printf("1. Toll Deduction\n");
-        printf("2. Show History by Date\n");
-        printf("3. Daily Receipt Summary\n");
-        printf("4. Exit\n");
-
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch(choice) 
-        {
-            case 1:
-                tollDeduction(date);
-                break;
-
-            case 2:
-                showHistoryByDate();
-                break;
-
-            case 3:
-                dailyReceipt();
-                break;
-
-            case 4:
-                printf("Exiting...\n");
-                break;
-
-            default:
-                printf("Invalid choice! Try again.\n");
-        }
-
-    } while(choice != 4);
-
-    return 0;
+return 0;
 }
