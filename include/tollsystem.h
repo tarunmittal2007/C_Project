d85bd8b62f54b204b95e6b73ec8fee1078ccb729
tarunmittal2007 <tarunmittal8572@gmail.com>
2025-11-28 00:09:45 +0530

    Update tollsystem.h

--- include/tollsystem.h
+++ include/tollsystem.h
@@ -1,16 +1,15 @@
 #ifndef TOLLSYSTEM_H
 #define TOLLSYSTEM_H
 
-// structure of toll record
-struct TollRecord {
-    char date[20];
-    char vehicleNumber[20];
-    float amount;
-    char paymentMode[20];
+struct VehicleRecord {
+char date[20];
+char vehicleNumber[20];
+float amount;
+char paymentMode[20];
 };
 
-void tollDeduction(char *date);
-void showHistoryByDate();
+void TollCollection(char *date);
+void ShowHistory();
 void dailyReceipt();
 
 #endif
